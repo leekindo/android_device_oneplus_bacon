aa9cc67a06a95176faa8d5e8a053ab14ea17714f
leekindo <leekindo@gmail.com>
2017-03-08 11:15:07 +0800

    bacon: Add TWRP support To build:   * Clone https://github.com/LineageOS/android_external_busybox     to external/busybox   * Clone https://github.com/omnirom/android_bootable_recovery     to bootable/recovery-twrp   * . build/envsetup.sh && breakfast bacon   * export WITH_TWRP=true && mka recoveryimage

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -179,6 +179,12 @@ TARGET_RECOVERY_FSTAB := $(PLATFORM_PATH)/rootdir/etc/fstab.bacon
 # RPC
 TARGET_NO_RPC := true
 
+# TWRP Support - Optional
+ifeq ($(WITH_TWRP),true)
+WITH_SU := true
+-include device/oneplus/bacon/twrp.mk
+endif
+
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
 
